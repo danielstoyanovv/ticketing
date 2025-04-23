d462360a20055fd66e86463f650c6eb2540cf7d7
danielstoyanovv <daniel.stoyanovv@gmail.com>
2025-04-23 10:11:24 +0300

    Create current-user route Create signin route Create signout route Create signup route Create skaffold configuration

---
+++ auth/src/routes/current-user.ts
@@ -0,0 +1,10 @@
+"use strict";
+
+import express, {Request, Response} from "express";
+
+const router = express.Router()
+router.get("/api/users/current-user", (req: Request, res: Response) => {
+    res.send("Hi there!")
+})
+
+export { router as currentUserRouter }
