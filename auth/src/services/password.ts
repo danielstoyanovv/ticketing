cf51db21d282cfc13ce1ab35c86bdc626657a415
danielstoyanovv <daniel.stoyanovv@gmail.com>
2025-04-28 17:41:48 +0300

    Update password.ts - add methods dock block

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -4,6 +4,12 @@ import { promisify } from 'util';
 const scryptAsync = promisify(scrypt);
 
 export class Password {
+
+    /**
+     * To hash
+     * @param {string} password
+     * @return {string}
+     */
     static async toHash(password: string) {
         const salt = randomBytes(8).toString('hex');
         const buf = (await scryptAsync(password, salt, 64)) as Buffer;
@@ -11,6 +17,12 @@ export class Password {
         return `${buf.toString('hex')}.${salt}`;
     }
 
+    /**
+     * compare
+     * @param {string} storedPassword
+     * @param {string} suppliedPassword
+     * @return {boolean}
+     */
     static async compare(storedPassword: string, suppliedPassword: string) {
         const [hashedPassword, salt] = storedPassword.split('.');
         const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
