1f1b3c9d33f551ec91c7c96ba2cefc21e515d35d
danielstoyanovv <daniel.stoyanovv@gmail.com>
2025-05-02 14:17:45 +0300

    Update auth.ts - UnauthorizedRequestError from logic - Delete UnauthorizedRequestError

--- auth/src/middlewares/auth.ts
+++ auth/src/middlewares/auth.ts
@@ -2,7 +2,6 @@
 
 import {Request, Response, NextFunction } from "express";
 import {TokenManager} from "../services/tokenManager";
-import {UnauthorizedRequestError} from "../errors/unauthorized-request-error";
 import {ForbiddenRequestError} from "../errors/forbidden-request-error";
 
 const tokenManager = new TokenManager()
@@ -11,9 +10,6 @@ export const auth = async (req: Request, res: Response, next: NextFunction) => {
         return next();
     }
     const token = req.session.jwt
-    // const token = req.body.token || req.query.token || req.headers['x-access-token'] || false
-    if (!token) throw new UnauthorizedRequestError("Token is missing in this request!")
-
     if (token) {
         const currentToken = tokenManager
             .setToken(token)
