2dee0357edbde04e0671030813a0dfc753d1ddb3
danielstoyanovv <daniel.stoyanovv@gmail.com>
2025-05-04 14:31:59 +0300

    Update users.test - move into integration folder Create users.test - add unit tests

---
+++ auth/src/__test__/unit/users.test.ts
@@ -0,0 +1,25 @@
+import {UserService} from "../../services/UserService";
+import {BadRequestError} from "../../errors/bad-request-error";
+import {Password} from "../../services/password";
+it("verify user email and password are correct", async () => {
+    const email = "test@test.com";
+    const password = await Password.toHash("password")
+
+    const service = new UserService()
+    const exists = await service
+        .setEmail(email)
+        .userExists()
+    if (exists) throw new BadRequestError("Email in use")
+    // Verify email
+    const user = await service
+        .setEmail(email)
+        .setPassword(password)
+        .createUser()
+    expect(user.email).toEqual(email);
+    // Verify password
+    const passwordsMatch =  await Password.compare(
+        user.password,
+        "password"
+    )
+    expect(passwordsMatch).toEqual(true);
+})
