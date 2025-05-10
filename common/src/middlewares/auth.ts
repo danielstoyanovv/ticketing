4845f5874b54b9dc604226e6adc7feceb100db2e
danielstoyanovv <daniel.stoyanovv@gmail.com>
2025-05-10 11:57:39 +0300

    Update auth.ts - throw new error if token is missing logic Add unauthorized-request-error.ts - declare class UnauthorizedRequestError - extends abstract class - add rules

--- common/src/middlewares/auth.ts
+++ common/src/middlewares/auth.ts
@@ -3,12 +3,11 @@
 import {Request, Response, NextFunction } from "express";
 import {TokenManager} from "../services/tokenManager";
 import {ForbiddenRequestError} from "../errors/forbidden-request-error";
+import {UnauthorizedRequestError} from "../errors/unauthorized-request-error";
 
 const tokenManager = new TokenManager()
 export const auth = async (req: Request, res: Response, next: NextFunction) => {
-    if (!req.session?.jwt) {
-        return next();
-    }
+    if (!req.session?.jwt) throw new UnauthorizedRequestError("Authentication token is not provided.")
     const token = req.session.jwt
     if (token) {
         const currentToken = tokenManager
