dda932aabcd66e8401f902246ad2af2394e21b45
danielstoyanovv <daniel.stoyanovv@gmail.com>
2025-05-15 16:11:21 +0300

    update listener.ts - extend configuration - add SIGINT logic - fix the wrong data type check - add SIGTERM logic

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -6,8 +6,16 @@ const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
 })
 stan.on("connect", () => {
     console.log("Listener connected to NATS")
+
+    stan.on("close", () => {
+        console.log("NATS connection closed")
+        process.exit()
+    })
+
     const options = stan.subscriptionOptions()
         .setManualAckMode(true)
+        .setDeliverAllAvailable()
+        .setDurableName("accounting-service")
     const subscription =  stan.subscribe(
         "ticket:created",
         "orders-service-queue-group",
@@ -16,9 +24,10 @@ stan.on("connect", () => {
     subscription.on("message", (msg: Message) => {
         const data = msg.getData()
 
-        if (typeof data === "string")
-            console.log(`Received event #${msg.getSequence()}, with data: ${data}`)
+        if (typeof data === String) console.log(`Received event #${msg.getSequence()}, with data: ${data}`)
 
         msg.ack()
     })
 })
+process.on("SIGINT", () => {stan.close()})
+process.on("SIGTERM", () => {stan.close()})
