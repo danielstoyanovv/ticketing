539fe65bec7e2b394e8c976e2819d0530a20e83f
danielstoyanovv <daniel.stoyanovv@gmail.com>
2025-05-15 18:09:14 +0300

    Add BaseListener abstract class - declare abstract onMessage method - declare abstract subject param - declare abstract queueGroupName param - add constructor logic - implement subscriptionOptions method - implement parseMessage method Update liste

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,33 +1,20 @@
-import nats, { Message } from "node-nats-streaming"
+import nats, { Message, Stan } from "node-nats-streaming"
 import {randomBytes} from "node:crypto";
+import {TicketCreatedListener} from "./events/ticket-created-listener";
+
 
 const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
     url: 'http://localhost:4222',
 })
 stan.on("connect", () => {
-    console.log("Listener connected to NATS")
+    console.log("BaseListener connected to NATS")
 
     stan.on("close", () => {
         console.log("NATS connection closed")
         process.exit()
     })
 
-    const options = stan.subscriptionOptions()
-        .setManualAckMode(true)
-        .setDeliverAllAvailable()
-        .setDurableName("accounting-service")
-    const subscription =  stan.subscribe(
-        "ticket:created",
-        "orders-service-queue-group",
-        options
-    )
-    subscription.on("message", (msg: Message) => {
-        const data = msg.getData()
-
-        if (typeof data === String) console.log(`Received event #${msg.getSequence()}, with data: ${data}`)
-
-        msg.ack()
-    })
+    new TicketCreatedListener(stan).listen()
 })
 process.on("SIGINT", () => {stan.close()})
 process.on("SIGTERM", () => {stan.close()})
