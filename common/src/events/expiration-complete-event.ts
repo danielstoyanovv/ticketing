49bbc75b8d5799ed2e58c529b70ccdb1ff9b7061
danielstoyanovv <daniel.stoyanovv@gmail.com>
2025-05-30 16:41:08 +0300

    Add ExpirationCompleteEvent interface - use Subjects for body logic Update index

---
+++ common/src/events/expiration-complete-event.ts
@@ -0,0 +1,10 @@
+"use strict";
+
+import {Subjects} from "./subjects";
+
+export interface ExpirationCompleteEvent {
+    subject: Subjects.ExpirationComplete,
+    data: {
+        orderId: string
+    }
+}
