2a5b900e1a47c254f02c0629e2fa130cd933121f
danielstoyanovv <daniel.stoyanovv@gmail.com>
2025-06-02 17:18:02 +0300

    Add payments service Add Docker files Add app.ts - configure express application Add data - add constants file Add index

---
+++ payments/src/app.ts
@@ -0,0 +1,28 @@
+"use strict";
+
+import express from "express"
+import "express-async-errors"
+require('dotenv').config()
+import helmet from "helmet";
+import cors from "cors"
+import {errorHandler} from "@dmstickets/common";
+import cookieSession from "cookie-session";
+
+export const app = express()
+
+app.set("trust proxy", false)
+app.use(
+    cookieSession({
+        signed: false,
+        secure: process.env.NODE_ENV !== "test"
+    })
+)
+
+app.use(express.json())
+
+app.use(helmet())
+
+app.use(cors())
+
+app.use(errorHandler)
+export default { app }
---
+++ payments/src/index.ts
@@ -0,0 +1,24 @@
+"use strict";
+
+require('dotenv').config()
+import {MongoDbConnect} from "./config/MongoDbConnect";
+import {app} from "./app"
+import {natsWrapper} from "./nats-wrapper";
+
+natsWrapper.connect("ticketing", "reehh", "http://nats-clusterip-srv:4222").then(result => {
+    result.client.on("close", () => {
+        console.log("NATS connection closed!")
+        process.exit()
+    })
+    process.on("SIGINT", () => {result.client.close()})
+    process.on("SIGTERM", () => {result.client.close()})
+}).catch(err => {
+    console.error(err)
+})
+
+const port = process.env.TICKETS_PORT || 5000
+
+const database = new MongoDbConnect()
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
